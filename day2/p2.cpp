cb83e47547aa05ef95483f43f603fa9a75f42d55
ibricchi <19757722+IBricchi@users.noreply.github.com>
2023-12-02 23:16:20 +0000

    day2

---
+++ day2/p2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+int main() {
+    // while input is available
+    int accumulator = 0;
+    int counter = 0;
+    while (true) {
+        counter++;
+        // read one line
+        std::string line;
+        std::getline(std::cin, line);
+        int pos = 0;
+
+        if (line.size() == 0) {
+            break;
+        }
+
+        // read until second space
+        int space_counter = 0;
+        while (space_counter < 2) {
+            if (line[pos] == ' ') {
+                space_counter++;
+            }
+            pos++;
+        }
+
+        // get the data for each section
+        int max_blue = 0, max_green = 0, max_red = 0;
+        int value = 0;
+        while (true) {
+            if (pos >= line.size()) {
+                break;
+            }
+            switch(line[pos]) {
+                case 'b':
+                    pos += 5;
+                    max_blue = std::max(max_blue, value);
+                    value = 0;
+                    break;
+                case 'g':
+                    pos += 6;
+                    max_green = std::max(max_green, value);                    
+                    value = 0;
+                    break;
+                case 'r':
+                    pos += 4;
+                    max_red = std::max(max_red, value);
+                    value = 0;
+                    break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    value = value * 10 + (line[pos] - '0');
+                    pos++;
+                    break;
+                default:
+                    pos++;
+                    break;
+            }
+
+        }
+
+        accumulator += max_blue * max_green * max_red;
+    }
+    std::cout << accumulator << std::endl;
+}
